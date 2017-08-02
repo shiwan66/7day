7b32d9e1e1326e8637e6590e0181a3f807f0852a
shiwan66 <shiwan66@gmail.com>
2017-08-02 21:19:31 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.BusinessLayer;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public class Customer
+        {
+            public string CustomerName { get; set; }
+            public string Address { get; set; }
+            public override string ToString()
+            {
+                return this.CustomerName + "|" + this.Address;
+            }
+        }
+        // GET: Test
+        public Customer GetString()
+        {
+            Customer c = new Customer { CustomerName = "Customer 1", Address = "Address1" };
+            return c;
+        }
+        [NonAction]
+        public string simpleMethod()
+        {
+            return "Hi, I am not action method";
+        }
+        public ActionResult Index()
+        {
+            //if(false)
+            //{
+            //    return Content("hello");
+            //}
+            //Employee emp = new Employee();
+            //emp.FirstName = "Sukesh";
+            //emp.LastName = "Marla";
+            //emp.Salary = 20000;
+            ////ViewData["Employee"] = emp;
+            ////ViewBag.Employee = emp;
+            //return View("MyView", emp);
+
+            //Employee emp = new Employee();
+            //emp.FirstName = "Sukesh";
+            //emp.LastName = "Marla";
+            //emp.Salary = 20000;
+
+            //EmployeeViewModel vmEmp = new EmployeeViewModel();
+            //vmEmp.EmployeeName = emp.FirstName + " " + emp.LastName;
+            //vmEmp.Salary = emp.Salary.ToString("C");
+            //if (emp.Salary > 15000)
+            //{
+            //    vmEmp.SalaryColor = "yellow";
+            //}
+            //else
+            //{
+            //    vmEmp.SalaryColor = "green";
+            //}
+
+            //vmEmp.UserName = "Admin";
+            //return View("MyView", vmEmp);
+
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach(Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel =
+                      new EmployeeViewModel();
+
+                empViewModel.EmployeeName =
+                      emp.FirstName + " " + emp.LastName;
+
+                empViewModel.Salary = emp.Salary.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            //employeeListViewModel.UserName = "Admin";
+            return View("Index", employeeListViewModel);
+
+        }
+    }
+}
