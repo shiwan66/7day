fffbe19618cd9d13f141910d0ae951ec21dccdd6
shiwan66 <shiwan66@gmail.com>
2017-08-08 00:51:58 +0800

    sync

---
+++ ViewModel/SPA/EmployeeListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WebApplication1.ViewModels.SPA
+{
+    public class EmployeeListViewModel
+    {
+        public List<EmployeeViewModel> Employees { get; set; }
+    }
+}
---
+++ WebApplication1/Areas/SPA/Controllers/MainController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.BusinessLayer;
+using WebApplication1.Models;
+using WebApplication1.ViewModels.SPA;
+using OldViewModel = WebApplication1.ViewModels;
+using WebApplication1.Filters;
+
+
+namespace WebApplication1.Areas.SPA.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: SPA/Main
+        public ActionResult Index()
+        {
+            MainViewModel v = new MainViewModel();
+            v.UserName = User.Identity.Name;
+            v.FooterData = new OldViewModel.FooterViewModel();
+            v.FooterData.CompanyName = "StepByStepSchools";//Can be set to dynamic value
+            v.FooterData.Year = DateTime.Now.Year.ToString();
+            return View("Index", v);
+        }
+        public ActionResult EmployeeList()
+        {
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            EmployeeBusinessLayer empBal = new EmployeeBusinessLayer();
+            List<Employee> employees = empBal.GetEmployees();
+
+            List<EmployeeViewModel> empViewModels = new List<EmployeeViewModel>();
+
+            foreach (Employee emp in employees)
+            {
+                EmployeeViewModel empViewModel = new EmployeeViewModel();
+                empViewModel.EmployeeName = emp.FirstName + " " + emp.LastName;
+                empViewModel.Salary = emp.Salary.Value.ToString("C");
+                if (emp.Salary > 15000)
+                {
+                    empViewModel.SalaryColor = "yellow";
+                }
+                else
+                {
+                    empViewModel.SalaryColor = "green";
+                }
+                empViewModels.Add(empViewModel);
+            }
+            employeeListViewModel.Employees = empViewModels;
+            return View("EmployeeList", employeeListViewModel);
+        }
+        public ActionResult GetAddNewLink()
+        {
+            if (Convert.ToBoolean(Session["IsAdmin"]))
+            {
+                return PartialView("AddNewLink");
+            }
+            else
+            {
+                return new EmptyResult();
+            }
+        }
+        [AdminFilter]
+        public ActionResult AddNew()
+        {
+            CreateEmployeeViewModel v = new CreateEmployeeViewModel();
+            return PartialView("CreateEmployee", v);
+        }
+    }
+}
